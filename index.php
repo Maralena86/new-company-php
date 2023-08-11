7bef82858897142d1f74f74512d1d97532cc0085
Maralena86 <mari16cobain@msn.com>
2023-08-11 21:23:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+
+    <?php include("templates/header.php")
+    ?>
+    <div>
+        <h1>This is the new company</h1>
+        <p></p>
+    </div>
+    <?php include("templates/footer.php");?>
---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+        </main>
+        <footer class="bg-gray-800">
+            <div class="relative flex h-16 items-center justify-between">
+                <p class="align-center text-white">The new build's company</p>
+            </div>
+        </footer>
+        </body>
+
+</html>
